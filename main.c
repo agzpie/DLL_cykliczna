eca0837b7542dd4dca747381ad42ec1fd3a57d33
agzpie <agata.pietralik@student.wat.edu.pl>
2018-04-16 02:25:42 +0200

    gotowe

--- main.c
+++ main.c
@@ -21,6 +21,7 @@ void push_last(thunderstruct *head_node, int value) {
 
         if (current->next == NULL) { //adding the second node with data
             current->next = malloc(sizeof(thunderstruct));
+            current->prev = current->next;
             current->next->value = value;
             current->next->next = current;
             current->next->prev = current;
@@ -33,34 +34,60 @@ void push_last(thunderstruct *head_node, int value) {
             current->next->value = value;
             current->next->next = head_node->next;
             current->next->prev = current;
+            head_node->next->prev = current->next;
         }
     }
 }
 
-/*void push_fr_start(thunderstruct *head_node, int value, int n) {   // n-ty rekord do przeniesienia
-    if (head_node->next == NULL)
-        return 1;
+void move(thunderstruct* head_node, int n, int k, char kier) {
+    thunderstruct* current;
+    if (kier == 'p') {
+        current = head_node->next;
+        while (n>0) {
+            current = current->next;
+            n--;
+        }
+        thunderstruct* moved_node = current;
+        current->prev->next = current->next;
+        current->next->prev = current->prev;
 
-
-    for (int i=0; i<=n; i++){
-
+        while (k>0) {
+            current = current->next;
+            k--;
+        }
+        moved_node->prev = current;
+        moved_node->next = current->next;
+        current->next->prev = moved_node;
+        current->next = moved_node;
     }
-*/
+    else {
+        current = head_node->next->prev;
+        while (n>0) {
+            current = current->prev;
+            n--;
+        }
+        thunderstruct* moved_node = current;
+        current->prev->next = current->next;
+        current->next->prev = current->prev;
 
+        while (k>0) {
+            current = current->prev;
+            k--;
+        }
+        moved_node->next = current;
+        moved_node->prev = current->prev;
+        current->prev->next = moved_node;
+        current->prev = moved_node;
+    }
+}
 
-//}
-
-
-
-
-
-int pop(thunderstruct *current) {
+/*int pop(thunderstruct *current) {
     int value = current->value;
     current->prev->next = current->next;
     current->next->prev = current->prev;
     free(current);
     return value;
-}
+}*/
 
 /*void clean(thunderstruct *head_node) {
 // TODO
@@ -86,6 +113,9 @@ void printall(thunderstruct *head_node) {
     }
 }
 
+/*void push_after(thunderstruct* node, int value) { // value - value added to the list
+}*/
+
 
 
 
@@ -104,16 +134,35 @@ int main(){
 
     // Randomize
     srand(time(NULL));
-    int max = 25;
-    int n = 5;
-    //printf("Enter nodes number: ");
-    //scanf("%d", &n);
-
+    int max = 99;
+    int m, n, k;
+    char kier;
+    printf("Ilosc node'ow:\n");
+    scanf("%d", &m);
+    printf("Ktory node przeniesc?\n");
+    scanf("%d", &n);
+    if (n<0 || m<=0) {
+        printf("Program nie zadziala poprawnie. Podaj odpowiednie liczby.\n");
+        return 1;
+    }
+    printf("O ile pozycji node ma byc przeniesiony?\n");
+    scanf("%d", &k);
+    printf("Podaj kierunek poruszania, gdzie k - od konca, p - od poczaktu:\n");
+    scanf(" %c", &kier);
+    //if (k<=0 || (kier != 'k' && kier != 'p')) {
+     //   printf("Zle parametry.\n");
+      //  return 1;
+    //}
     // Create nodes with random values
-    for(int i=0; i<n;i++)
+    for (int i=0; i<m; i++)
         push_last(head_node, (rand() % max+1));
 
-    printf("\nList:\n");
+    printf("\nLista:\n");
+    printall(head_node);
+
+    move(head_node, n, k, kier);
+
+    printf("\nZmieniona lista:\n");
     printall(head_node);
 
     return 0;
